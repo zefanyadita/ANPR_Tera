e3d48858679f9fca9b93281bce8350683e5cfa44
zefanyadita <147527401+zefanyadita@users.noreply.github.com>
2023-10-11 12:22:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import pytesseract
 import numpy as np
 from pytesseract import image_to_string
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
-
 def main():
     st.title("Automatic Number Plate Recognition Web-App")
     activities = ["About", "Detection"]
